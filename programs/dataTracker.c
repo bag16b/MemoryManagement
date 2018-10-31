906114b1344e51b6b24086acae95af7ec545abcc
bag16b <bag16b@acu.edu>
2018-10-30 23:07:53 -0500

    dataTracker now does something

--- programs/dataTracker.c
+++ programs/dataTracker.c
@@ -5,7 +5,6 @@
 #include <sys/utsname.h>
 #include <minix/com.h>
 #include <minix/config.h>
-#include <minix/sysinfo.h>
 #include <minix/type.h>
 #include <minix/vm.h>
 #include <string.h>
@@ -14,7 +13,17 @@
 #include "mproc.h"
 #include "param.h"
 
+int isPrime(int x)
+{
+    printf("%d", 1);
+    return 1;
+ }
+
 
 PUBLIC int main() {
-
+    while(true){
+        isPrime(0);
+        sleep(1);
+    }
+    
 }
